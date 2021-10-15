dec804fd082a6ece968876771b4c126221fcf0b5
blazingsss <blazings@vip.qq.com>
2021-10-15 17:56:11 +0800

    add my observer

---
+++ src/main/java/com/blazings/suanfa/DesignPattern/Adapter/caipiao/ObjectFor3D.java
@@ -0,0 +1,40 @@
+package com.blazings.suanfa.DesignPattern.Adapter.caipiao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectFor3D implements SubjectI{
+
+    List<ObserverI> observers = new ArrayList<ObserverI>();
+
+    String msg;
+
+    /**
+     * @param observerI registerObserver
+     */
+    @Override
+    public void registerObserver(ObserverI observerI) {
+        observers.add(observerI);
+    }
+
+    @Override
+    public void unregisterObserver(ObserverI observerI) {
+        int indexOf = observers.indexOf(observerI);
+        if (indexOf >= 0) {
+            observers.remove(indexOf);
+        }
+    }
+
+    @Override
+    public void notifyObserver() {
+        for (ObserverI observer : observers) {
+            observer.update(msg);
+        }
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+
+        notifyObserver();
+    }
+}
