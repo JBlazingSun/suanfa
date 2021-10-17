792fd1ba0240fab7820e29d18b6a9972b0eb3a25
JBlazingSun <blazings@vip.qq.com>
2021-10-17 18:54:35 +0800

    add

--- src/main/java/com/blazings/suanfa/DesignPattern/Adapter/SpringObserver/ReaderListener.java
+++ src/main/java/com/blazings/suanfa/DesignPattern/Adapter/SpringObserver/ReaderListener.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ReaderListener implements ApplicationListener<MyEvent>{
-    String name;
     String article;
 
     @Override
@@ -15,23 +14,5 @@ public class ReaderListener implements ApplicationListener<MyEvent>{
 
     private void UpdateArticle(MyEvent myEvent) {
         this.article = myEvent.DoSomething();
-        System.out.printf("我是读者： %s，文章已更新为：  %s", this.name, this.article);
-        System.out.println("");
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getArticle() {
-        return article;
-    }
-
-    public void setArticle(String article) {
-        this.article = article;
     }
 }
