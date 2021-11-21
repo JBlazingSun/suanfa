b9e63436c7ed80b363a7c6fb8dddd16702c14d10
blazingsss <blazings@vip.qq.com>
2021-11-21 15:03:33 +0800

    add

---
+++ src/test/java/com/blazings/suanfa/JavaPractice/Java8LamdbaTest.java
@@ -0,0 +1,25 @@
+package com.blazings.suanfa.JavaPractice;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Java8LamdbaTest {
+
+    @Test()
+    void showPredicate() {
+        Java8Lamdba.ShowPredicate();
+    }
+
+    @Test
+    void showPredicateFilter() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
