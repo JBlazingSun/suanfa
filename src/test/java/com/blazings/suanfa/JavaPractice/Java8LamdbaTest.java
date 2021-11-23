961d97de269924b8b2d230992476b9facbdb12ef
JBlazingSun <blazings@vip.qq.com>
2021-11-23 23:00:03 +0800

    add

--- src/test/java/com/blazings/suanfa/JavaPractice/Java8LamdbaTest.java
+++ src/test/java/com/blazings/suanfa/JavaPractice/Java8LamdbaTest.java
@@ -1,25 +1,43 @@
 package com.blazings.suanfa.JavaPractice;
 
+import com.alibaba.fastjson.JSON;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 class Java8LamdbaTest {
 
-    @Test()
-    void showPredicate() {
-        Java8Lamdba.ShowPredicate();
-    }
+  @Test()
+  void showPredicate() {
+    Java8Lamdba.ShowPredicate();
+  }
 
-    @Test
-    void showPredicateFilter() {
-    }
+  @Test
+  void showPredicateFilter() {
+    List<String> list3 = Arrays.asList("zhangSan", "liSi", "wangWu");
+    System.out.println("转换之前的数据:" + list3);
+    Predicate<String> stringPredicate = s -> {
+      s+="后面加的";
+      return s.contains("A") && s.contains("S");
+    };
+    List<String> stringList =
+        list3.stream()
+            .map(s -> s.toUpperCase())
+            .filter(s -> stringPredicate.test(s))
+            .collect(Collectors.toList());
+    System.out.println(stringList);
+  }
 
-    @BeforeEach
-    void setUp() {
-    }
+  @BeforeEach
+  void setUp() {}
 
-    @AfterEach
-    void tearDown() {
-    }
+  @AfterEach
+  void tearDown() {}
 }
