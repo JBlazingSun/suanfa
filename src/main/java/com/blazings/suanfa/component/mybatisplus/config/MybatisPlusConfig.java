47c9fc95febeb0f658c2bb7e3547374ebc47b9c7
JBlazingSun <blazings@vip.qq.com>
2021-11-29 14:14:59 +0800

    add

--- src/main/java/com/blazings/suanfa/component/mybatisplus/config/MybatisPlusConfig.java
+++ src/main/java/com/blazings/suanfa/component/mybatisplus/config/MybatisPlusConfig.java
@@ -1,10 +1,15 @@
 package com.blazings.suanfa.component.mybatisplus.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MybatisPlusConfig {
-
+    // 旧版
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
 }
