2fc10918a2fe2f978cf227540366c9f95b8f933d
JBlazingSun <blazings@vip.qq.com>
2021-11-29 12:18:10 +0800

    add

---
+++ src/main/java/com/blazings/suanfa/component/mybatisplus/config/MybatisPlusConfig.java
@@ -0,0 +1,10 @@
+package com.blazings.suanfa.component.mybatisplus.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+}
---
+++ src/main/java/com/blazings/suanfa/component/mybatisplus/service/user_nameService.java
@@ -0,0 +1,7 @@
+package com.blazings.suanfa.component.mybatisplus.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.blazings.suanfa.component.mybatisplus.vo.user_name;
+
+public interface user_nameService extends IService<user_name>{
+}
