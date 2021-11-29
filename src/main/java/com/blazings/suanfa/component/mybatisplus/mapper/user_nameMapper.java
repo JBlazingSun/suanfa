2a58ebd7a13eafdeee5b29bd942094752b9806d8
JBlazingSun <blazings@vip.qq.com>
2021-11-29 12:18:10 +0800

    add

--- src/main/java/com/blazings/suanfa/component/mybatisplus/mapper/user_nameMapper.java
+++ src/main/java/com/blazings/suanfa/component/mybatisplus/mapper/user_nameMapper.java
@@ -2,6 +2,8 @@ package com.blazings.suanfa.component.mybatisplus.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.blazings.suanfa.component.mybatisplus.vo.user_name;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface user_nameMapper extends BaseMapper<user_name> {
 }
--- src/test/java/com/blazings/suanfa/component/mybatisplus/mapper/user_nameMapperTest.java
+++ src/test/java/com/blazings/suanfa/component/mybatisplus/mapper/user_nameMapperTest.java
@@ -3,6 +3,7 @@ package com.blazings.suanfa.component.mybatisplus.mapper;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.TimeInterval;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.blazings.suanfa.component.mybatisplus.service.impl.user_nameServiceImpl;
 import com.blazings.suanfa.component.mybatisplus.vo.user_name;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ class user_nameMapperTest {
     @Autowired
     user_nameMapper user_nameMapper;
 
+    @Autowired
+    user_nameServiceImpl user_nameService;
+
     @Test
     public void test() {
         TimeInterval timer = DateUtil.timer();
@@ -26,5 +30,10 @@ class user_nameMapperTest {
 
         System.out.println(timer.interval());
         System.out.println(user_nameList);
+    }
+
+    @Test
+    void user_nameServiceImplTest() {
+        System.out.println("user_nameService.count() = " + user_nameService.count());
     }
 }
