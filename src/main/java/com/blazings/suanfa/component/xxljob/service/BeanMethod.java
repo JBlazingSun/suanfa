e8115d346b11a4cc55f05de375561ba85ebe18a7
JBlazingSun <blazings@vip.qq.com>
2021-12-02 16:52:39 +0800

    add

---
+++ src/main/java/com/blazings/suanfa/component/xxljob/service/BeanMethod.java
@@ -0,0 +1,16 @@
+package com.blazings.suanfa.component.xxljob.service;
+
+import cn.hutool.core.date.DateUtil;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BeanMethod {
+
+    @XxlJob("BeanMethoddemoJobHandler")
+    public void demoJobHandler() throws Exception{
+        XxlJobHelper.log("XXL-JOB, Hello World.");
+        System.out.println("demoJobHandler "+ DateUtil.now());
+    }
+}
