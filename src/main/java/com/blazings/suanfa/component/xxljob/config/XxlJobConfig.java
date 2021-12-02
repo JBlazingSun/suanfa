a8d6ac728523c34db539e19b476476092f4b05eb
JBlazingSun <blazings@vip.qq.com>
2021-12-02 16:52:39 +0800

    add

--- src/main/java/com/blazings/suanfa/component/xxljob/config/XxlJobConfig.java
+++ src/main/java/com/blazings/suanfa/component/xxljob/config/XxlJobConfig.java
@@ -1,43 +1,47 @@
 package com.blazings.suanfa.component.xxljob.config;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashSet;
+
 @Configuration
 public class XxlJobConfig {
-  @Value("${xxl.job.admin.addresses}")
-  String adminAddresses;
+    @Value("${xxl.job.admin.addresses}")
+    String adminAddresses;
 
-  @Value("${xxl.job.accessToken}")
-  String accessToken;
-  //
-  @Value("${xxl.job.executor.appname}")
-  String appname;
-  //
-  //  @Value("${xxl.job.executor.ip}")
-  //  String ip;
-  //
-  //  @Value("${xxl.job.executor.port}")
-  //  int port;
-  //
-  //  @Value("${xxl.job.executor.logpath}")
-  //  String logPath;
-  //
-  //  @Value("${xxl.job.executor.logretentiondays}")
-  //  int logRetentionDays;
+    @Value("${xxl.job.accessToken}")
+    String accessToken;
 
-  @Bean
-  public XxlJobSpringExecutor xxlJobExecutor() {
-    XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
-    xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
-    xxlJobSpringExecutor.setAppname(appname);
-    //    xxlJobSpringExecutor.setIp(ip);
-    //    xxlJobSpringExecutor.setPort(port);
-    xxlJobSpringExecutor.setAccessToken(accessToken);
-    //    xxlJobSpringExecutor.setLogPath(logPath);
-    //    xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
-    return xxlJobSpringExecutor;
-  }
+    @Value("${xxl.job.executor.appname}")
+    String appname;
+
+    @Value("${xxl.job.executor.ip}")
+    String ip;
+
+    @Value("${xxl.job.executor.port}")
+    int port;
+
+    //  @Value("${xxl.job.executor.logpath}")
+    //  String logPath;
+    //
+    //  @Value("${xxl.job.executor.logretentiondays}")
+    //  int logRetentionDays;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        //    xxlJobSpringExecutor.setLogPath(logPath);
+        //    xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        return xxlJobSpringExecutor;
+    }
 }
