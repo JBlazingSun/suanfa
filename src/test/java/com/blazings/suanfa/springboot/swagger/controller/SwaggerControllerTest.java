0d9fbd13d0d36f019dcfea5c0e09bf70665ee2e7
JBlazingSun <blazings@vip.qq.com>
2021-12-11 16:31:35 +0800

    add xxljob

--- src/test/java/com/blazings/suanfa/springboot/swagger/controller/SwaggerControllerTest.java
+++ src/test/java/com/blazings/suanfa/springboot/swagger/controller/SwaggerControllerTest.java
@@ -2,13 +2,20 @@ package com.blazings.suanfa.springboot.swagger.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class SwaggerControllerTest {
+  MockMvc mockMvc;
+  @Autowired WebApplicationContext webApplicationContext;
 
     @BeforeEach
     void setUp() {
+    MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
