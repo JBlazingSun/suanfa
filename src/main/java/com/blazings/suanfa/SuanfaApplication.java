d438dffba3ce019d1dc93e6297a944fcacd5c963
JBlazingSun <blazings@vip.qq.com>
2021-12-12 22:08:53 +0800

    add xxljob

--- src/main/java/com/blazings/suanfa/SuanfaApplication.java
+++ src/main/java/com/blazings/suanfa/SuanfaApplication.java
@@ -14,38 +14,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com/blazings/suanfa/sql/tujie/mapper")
 public class SuanfaApplication implements CommandLineRunner {
 
-    @Autowired
-    MyPublisher myPublisher;
+  @Autowired MyPublisher myPublisher;
 
-    @Autowired
-    user_nameMapper user_nameMapper;
+  @Autowired user_nameMapper user_nameMapper;
 
-    @Autowired
-    CourseMapper courseMapper;
+  @Autowired CourseMapper courseMapper;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SuanfaApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SuanfaApplication.class, args);
+  }
 
-    @Override
-    public void run(String... args) throws Exception {
-//        String s = "a, ,efedsfs,   ddf,";
-//        List<String> split = StrSplitter.split(s,",",0,true,true,true);
-//        String join = Joiner.on(",").join(split);
-//        System.out.println(join);
-//        MyFastjson.Fun();
-//        Java8Lamdba.ShowPredicate();
+  @Override
+  public void run(String... args) throws Exception {
+    //        String s = "a, ,efedsfs,   ddf,";
+    //        List<String> split = StrSplitter.split(s,",",0,true,true,true);
+    //        String join = Joiner.on(",").join(split);
+    //        System.out.println(join);
+    //        MyFastjson.Fun();
+    //        Java8Lamdba.ShowPredicate();
 
-//    System.out.println("SpringObserver");
-//    myPublisher.PublisherEvent("消息1111");
-//    myPublisher.PublisherEvent("消息22222");
-//
-//    System.out.println("caipiao");
-//    ObjectFor3D objectFor3D = new ObjectFor3D();
-//    Observer1 observer1 = new Observer1(objectFor3D);
-//    Observer2 observer2 = new Observer2(objectFor3D);
-//    // -------
-//    objectFor3D.setMsg("今天的号码是 17 22 42");
-//    objectFor3D.setMsg("明天的号码  12305 ");
-    }
+    //    System.out.println("SpringObserver");
+    //    myPublisher.PublisherEvent("消息1111");
+    //    myPublisher.PublisherEvent("消息22222");
+    //
+    //    System.out.println("caipiao");
+    //    ObjectFor3D objectFor3D = new ObjectFor3D();
+    //    Observer1 observer1 = new Observer1(objectFor3D);
+    //    Observer2 observer2 = new Observer2(objectFor3D);
+    //    // -------
+    //    objectFor3D.setMsg("今天的号码是 17 22 42");
+    //    objectFor3D.setMsg("明天的号码  12305 ");
+  }
 }
