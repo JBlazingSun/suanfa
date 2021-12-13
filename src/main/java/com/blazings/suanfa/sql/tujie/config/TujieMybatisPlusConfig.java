847fb6b4f119456f814411279461b7bdc3548049
JBlazingSun <blazings@vip.qq.com>
2021-12-13 18:07:27 +0800

    add

---
+++ src/main/java/com/blazings/suanfa/sql/tujie/config/TujieMybatisPlusConfig.java
@@ -0,0 +1,9 @@
+package com.blazings.suanfa.sql.tujie.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com/blazings/suanfa/sql/tujie/mapper")
+public class TujieMybatisPlusConfig {
+}
--- src/test/java/com/blazings/suanfa/SuanfaApplicationTests.java
+++ src/test/java/com/blazings/suanfa/SuanfaApplicationTests.java
@@ -2,6 +2,7 @@ package com.blazings.suanfa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootTest
 class SuanfaApplicationTests {
