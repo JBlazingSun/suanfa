f6c6eef5d830757b9079a73349e83747d54b01b1
JBlazingSun <blazings@vip.qq.com>
2021-12-21 17:36:29 +0800

    add

---
+++ src/main/java/com/blazings/suanfa/springboot/springmvc/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.blazings.suanfa.springboot.springmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.permitAll()
+			.and()
+			.logout()
+			////配置不需要登录验证
+			.permitAll();
+	}
+}
---
+++ src/test/java/com/blazings/suanfa/springboot/springmvc/controller/SpringSecurityTest.java
@@ -0,0 +1,37 @@
+package com.blazings.suanfa.springboot.springmvc.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+@SpringBootTest
+class SpringSecurityTest {
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+	MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() {
+		mockMvc=MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void hello() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/restful/hello")
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3));
+	}
+}
