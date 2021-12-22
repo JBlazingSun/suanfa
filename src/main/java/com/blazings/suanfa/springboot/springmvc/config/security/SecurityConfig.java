15b4dfa746a1f9a167d36919913d2b57a7d07d3e
JBlazingSun <blazings@vip.qq.com>
2021-12-22 23:22:52 +0800

    add

--- src/main/java/com/blazings/suanfa/springboot/springmvc/config/security/SecurityConfig.java
+++ src/main/java/com/blazings/suanfa/springboot/springmvc/config/security/SecurityConfig.java
@@ -2,19 +2,15 @@ package com.blazings.suanfa.springboot.springmvc.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest()
-			.permitAll()
-			.and()
-			.logout()
-			////配置不需要登录验证
-			.permitAll();
-	}
+    http.authorizeRequests().anyRequest().authenticated();
+    //			.and()
+    //			.logout()
+    //			////配置不需要登录验证
+    //			.permitAll();
+  }
 }
