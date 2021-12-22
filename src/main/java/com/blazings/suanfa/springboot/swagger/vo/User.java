c25c0a6a2158916bfeca530788abbfae135cc07d
JBlazingSun <blazings@vip.qq.com>
2021-12-22 16:24:11 +0800

    add

--- src/main/java/com/blazings/suanfa/springboot/swagger/vo/User.java
+++ src/main/java/com/blazings/suanfa/springboot/swagger/vo/User.java
@@ -1,11 +1,11 @@
 package com.blazings.suanfa.springboot.swagger.vo;
 
-import io.swagger.annotations.ApiModelProperty;
+//import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class User {
-  @ApiModelProperty(example = "yilidan")
+//  @ApiModelProperty(example = "yilidan")
   String name;
 
   int age;
