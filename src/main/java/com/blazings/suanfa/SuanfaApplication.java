ff51fd9e4a76e1ee4469491473f4128e376f069c
JBlazingSun <blazings@vip.qq.com>
2022-01-19 17:07:25 +0800

    add

--- src/main/java/com/blazings/suanfa/SuanfaApplication.java
+++ src/main/java/com/blazings/suanfa/SuanfaApplication.java
@@ -1,10 +1,8 @@
 package com.blazings.suanfa;
 
 import cn.hutool.cron.CronUtil;
-import cn.hutool.cron.task.Task;
 import com.blazings.suanfa.designpattern.observer.SpringObserver.MyPublisher;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
