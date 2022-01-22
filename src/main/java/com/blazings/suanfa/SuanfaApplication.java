bfeef0916d742e94d91cdb5ea7379e197c515f00
blazingsss <blazings@vip.qq.com>
2022-01-22 23:12:41 +0800

    add

--- src/main/java/com/blazings/suanfa/SuanfaApplication.java
+++ src/main/java/com/blazings/suanfa/SuanfaApplication.java
@@ -1,20 +1,27 @@
 package com.blazings.suanfa;
 
 import cn.hutool.cron.CronUtil;
-import com.blazings.suanfa.designpattern.observer.SpringObserver.MyPublisher;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RAtomicDouble;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.Resource;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @Slf4j
 public class SuanfaApplication implements CommandLineRunner {
 
-  @Resource
-  MyPublisher myPublisher;
+//  @Resource
+//  MyPublisher myPublisher;
+
+//  @Resource
+//  RedissonClient client;
 
   public static void main(String[] args) {
     SpringApplication.run(SuanfaApplication.class, args);
@@ -22,8 +29,13 @@ public class SuanfaApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-
     CronUtil.setMatchSecond(true);
     CronUtil.start();
+
+    Config config = Config.fromYAML(new File("src/main/resources/redisson.yaml"));
+    RedissonClient client = Redisson.create(config);
+    RAtomicDouble atomicDouble = client.getAtomicDouble("myDouble");
+    atomicDouble.getAndIncrement();
+    atomicDouble.expire(10, TimeUnit.MINUTES);
   }
 }
---
+++ src/main/java/com/blazings/suanfa/config/RedissonConfig.java
@@ -0,0 +1,18 @@
+package com.blazings.suanfa.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.io.File;
+import java.io.IOException;
+
+//@Configuration
+public class RedissonConfig {
+//	@Bean
+	public RedissonClient redisson() throws IOException {
+		// 本例子使用的是yaml格式的配置文件，读取使用Config.fromYAML，如果是Json文件，则使用Config.fromJSON
+		Config config = Config.fromYAML(new File("src/main/resources/redisson.yaml"));
+		return Redisson.create(config);
+	}
+}
