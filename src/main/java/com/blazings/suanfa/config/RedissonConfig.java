57229ff5926bf9804730ce9feaf070fa91418a19
JBlazingSun <blazings@vip.qq.com>
2022-01-24 22:39:40 +0800

    add

--- src/main/java/com/blazings/suanfa/config/RedissonConfig.java
+++ src/main/java/com/blazings/suanfa/config/RedissonConfig.java
@@ -1,20 +1,20 @@
-//package com.blazings.suanfa.config;
-//
-//import org.redisson.Redisson;
-//import org.redisson.api.RedissonClient;
-//import org.redisson.config.Config;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//@Configuration
-//public class RedissonConfig {
-//	@Bean
-//	public RedissonClient redisson() throws IOException {
-//		// 本例子使用的是yaml格式的配置文件，读取使用Config.fromYAML，如果是Json文件，则使用Config.fromJSON
-//		Config config = Config.fromYAML(new File("src/main/resources/redisson.yaml"));
-//		return Redisson.create(config);
-//	}
-//}
+package com.blazings.suanfa.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+
+@Configuration
+public class RedissonConfig {
+  @Bean
+  public RedissonClient redisson() throws IOException {
+    // 本例子使用的是yaml格式的配置文件，读取使用Config.fromYAML，如果是Json文件，则使用Config.fromJSON
+    Config config = Config.fromYAML(new File("src/main/resources/redisson.yaml"));
+    return Redisson.create(config);
+  }
+}
