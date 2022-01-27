d708cdb190a47bd42c21ebf1b217c8bd85507032
JBlazingSun <blazings@vip.qq.com>
2022-01-27 17:11:30 +0800

    add

--- mall/src/main/java/com/blazings/mall/config/security/SecurityConfig.java
+++ mall/src/main/java/com/blazings/mall/config/security/SecurityConfig.java
@@ -1,15 +1,16 @@
-package com.blazings.mall.config.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest()
-			.permitAll();
-	}
-}
+// package com.blazings.mall.config.security;
+//
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import
+// org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// @Configuration
+// public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.anyRequest()
+//			.permitAll();
+//	}
+// }
