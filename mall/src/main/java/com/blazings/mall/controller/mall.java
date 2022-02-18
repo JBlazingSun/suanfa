c8da3724bc6f3bd3676ff5bf5f2de9e456f805a9
JBlazingSun <blazings@vip.qq.com>
2022-02-19 01:10:33 +0800

    add

--- mall/src/main/java/com/blazings/mall/controller/mall.java
+++ mall/src/main/java/com/blazings/mall/controller/mall.java
@@ -1,4 +1,4 @@
-package com.blazings.mall.controller;
+package src.main.java.com.blazings.mall.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
