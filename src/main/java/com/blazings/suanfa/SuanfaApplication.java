d205e43bc5d67e318e0dae2f9bb6562da0e62f90
JBlazingSun <blazings@vip.qq.com>
2022-03-11 15:27:06 +0800

    add

--- src/main/java/com/blazings/suanfa/SuanfaApplication.java
+++ src/main/java/com/blazings/suanfa/SuanfaApplication.java
@@ -1,6 +1,5 @@
 package com.blazings.suanfa;
 
-import cn.hutool.cron.CronUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.CommandLineRunner;
@@ -24,14 +23,6 @@ public class SuanfaApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    CronUtil.setMatchSecond(true);
-    CronUtil.start();
-
-    //    Config config = Config.fromYAML(new File("src/main/resources/redisson.yml"));
-    //    RedissonClient client = Redisson.create(config);
-    //    RAtomicDouble atomicDouble = client.getAtomicDouble("myDouble");
-    //    atomicDouble.getAndIncrement();
-    //    atomicDouble.expire(10, TimeUnit.MINUTES);
     log.info(client.getConfig().toYAML());
   }
 
