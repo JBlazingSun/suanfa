4b46aa12a316fe443d4f7ea9c779b73c45887f5e
JBlazingSun <blazings@vip.qq.com>
2022-03-14 18:32:15 +0800

    add

--- nacos-consumer/src/main/java/com/example/nacosconsumer/controller/Consumer.java
+++ nacos-consumer/src/main/java/com/example/nacosconsumer/controller/Consumer.java
@@ -4,17 +4,26 @@ import com.example.nacosconsumer.feign.Provider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("consumer")
 public class Consumer {
 	@Autowired
 	Provider provider;
 
-	@GetMapping("doConsumer")
-	public String PublicMethod() {
+	@GetMapping("get")
+	public String GetMethod() {
+		String consumerGet = provider.Get();
+		return consumerGet;
+	}
 
-		return "";
+	@PostMapping("post")
+	public String PostMethod() {
+		String post = provider.Post("我是consumer post");
+		return post;
 	}
 }
