89f7d0c745d639a2d93f533a1fdd09c2a895b681
JBlazingSun <blazings@vip.qq.com>
2022-06-05 16:51:04 +0800

    Changes

---
+++ src/main/java/com/blazings/suanfa/component/junit5/Junit5J.java
@@ -0,0 +1,10 @@
+package com.blazings.suanfa.component.junit5;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Junit5J {
+	public int add(int i, int junit5J) {
+		return i+junit5J;
+	}
+}
---
+++ src/test/java/com/blazings/suanfa/component/junit5/junit5JTest.java
@@ -0,0 +1,16 @@
+package com.blazings.suanfa.component.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class junit5JTest {
+
+	@Autowired
+	Junit5J junit5J;
+	@Test
+	void add() {
+		int add = junit5J.add(1, 2);
+	}
+}
