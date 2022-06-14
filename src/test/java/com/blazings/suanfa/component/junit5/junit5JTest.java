82d3bb08c3fb2b332c849673367d2f80f94d1373
JBlazingSun <blazings@vip.qq.com>
2022-06-14 15:37:59 +0800

    Changes

--- src/test/java/com/blazings/suanfa/component/junit5/junit5JTest.java
+++ src/test/java/com/blazings/suanfa/component/junit5/junit5JTest.java
@@ -1,22 +1,35 @@
 package com.blazings.suanfa.component.junit5;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class junit5JTest {
 
 	@Autowired
 	Junit5J junit5J;
 	@Test
+	@DisplayName("add111111")
 	void add() {
 		int add = junit5J.add(1, 2);
+		Assertions.assertEquals(3,add);
+//		assertEquals(3,add);
 
 	}
 	@Test
-	void add2() {
-		int add = junit5J.add(3, 3);
-
+	@DisplayName("add22222")
+	@ParameterizedTest(name = "{0} + {1} = {2}")
+	@CsvSource({
+		"1,2,3",
+		"3,5,8",
+		"1,1,2"
+	})
+	void add2(int one, int two, int result) {
+		int add = junit5J.add(one, two);
+		Assertions.assertEquals(result,add);
 	}
 }
