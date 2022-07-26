94c189f89cd80007647f6294cfb2ebc316fe239b
JBlazingSun <blazing@vip.qq.com>
2022-07-26 21:37:46 +0800

    Changes

--- src/main/java/com/blazings/suanfa/component/junit5/MyMockito.java
+++ src/main/java/com/blazings/suanfa/component/junit5/MyMockito.java
@@ -3,8 +3,11 @@ package com.blazings.suanfa.component.junit5;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Mockito {
+public class MyMockito {
 	public static int add(int i, int junit5J) {
 		return i+junit5J;
+	}
+
+	public static void add() {
 	}
 }
