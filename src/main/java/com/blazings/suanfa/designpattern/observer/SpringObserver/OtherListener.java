7de905d58d2766e4ab3d293db40c73ca9af225b1
JBlazingSun <blazings@vip.qq.com>
2022-07-26 11:00:10 +0800

    Changes

--- src/main/java/com/blazings/suanfa/designpattern/observer/SpringObserver/OtherListener.java
+++ src/main/java/com/blazings/suanfa/designpattern/observer/SpringObserver/OtherListener.java
@@ -1,12 +1,14 @@
 package com.blazings.suanfa.designpattern.observer.SpringObserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class OtherListener implements ApplicationListener<MyEvent> {
     @Override
     public void onApplicationEvent(MyEvent myEvent) {
-    System.out.println("OtherListener 听到了   "+ myEvent.msg);
+        log.info("OtherListener 听到了   "+ myEvent.msg);
     }
 }
