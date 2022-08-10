254da7c1a6aa1a11e921f53400498427b47c38c9
JBlazingSun <blazings@vip.qq.com>
2022-08-10 20:35:02 +0800

    update

--- src/main/java/com/blazings/suanfa/component/retrofit/HelloRetrofitClient.java
+++ src/main/java/com/blazings/suanfa/component/retrofit/HelloRetrofitClient.java
@@ -4,6 +4,13 @@ import com.github.lianjiatech.retrofit.spring.boot.core.RetrofitClient;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
+/**
+ * taobao:
+ *   baseUrl: http://api.m.taobao.com/rest/
+ *
+ * #taobao time api
+ * #http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp
+ */
 @RetrofitClient(baseUrl = "${taobao.baseUrl}")
 public interface HelloRetrofitClient {
 	@GET("api3.do")
