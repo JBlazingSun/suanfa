1a0837d299a88a958826268e4f01390ba6f8c637
JBlazingSun <blazings@vip.qq.com>
2022-08-23 15:40:47 +0800

    add

--- src/test/java/com/blazings/suanfa/component/retrofit/HelloRetrofitClientTest.java
+++ src/test/java/com/blazings/suanfa/component/retrofit/HelloRetrofitClientTest.java
@@ -4,7 +4,6 @@ import com.blazings.suanfa.entity.TaobaoTime;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
