d12fa2a29379a07422a24bd02edbe41c070d3599
JBlazingSun <blazings@vip.qq.com>
2022-08-26 23:21:54 +0800

    update

--- src/main/java/com/blazings/suanfa/component/retrofit/DownloadRetrofitClient.java
+++ src/main/java/com/blazings/suanfa/component/retrofit/DownloadRetrofitClient.java
@@ -4,10 +4,12 @@ import com.github.lianjiatech.retrofit.spring.boot.core.RetrofitClient;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
+
+import java.util.Map;
 
 @RetrofitClient(baseUrl = "${tuna.baseUrl}")
 public interface DownloadRetrofitClient {
-	@GET("")
-	Response<ResponseBody> download(@Path("u") String u);
+	@GET("it/u=4276151928,2257626089&fm=253&fmt=auto&app=138&f=JPEG")
+	Response<ResponseBody> download(@QueryMap Map<String,String> params);
 }
--- src/test/java/com/blazings/suanfa/component/retrofit/DownloadRetrofitClientTest.java
+++ src/test/java/com/blazings/suanfa/component/retrofit/DownloadRetrofitClientTest.java
@@ -1,6 +1,8 @@
 package com.blazings.suanfa.component.retrofit;
 
+import com.google.common.collect.Maps;
 import okhttp3.ResponseBody;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import retrofit2.Response;
@@ -10,7 +12,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.UUID;
+import java.util.HashMap;
 
 @SpringBootTest
 class DownloadRetrofitClientTest {
@@ -19,7 +21,15 @@ class DownloadRetrofitClientTest {
 
 	@Test
 	void download() throws IOException {
-		Response<ResponseBody> response = downloadRetrofitClient.download("4276151928,2257626089&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=313");
+		HashMap<@Nullable String, @Nullable String> hashMap = Maps.newHashMap();
+//		hashMap.put("u", "4276151928,2257626089");
+//		hashMap.put("fm", "253");
+//		hashMap.put("fmt", "auto");
+//		hashMap.put("app", "138");
+//		hashMap.put("f", "JPEG");
+		hashMap.put("w", "500");
+		hashMap.put("h", "313");
+		Response<ResponseBody> response = downloadRetrofitClient.download(hashMap);
 		ResponseBody responseBody = response.body();
 		// 二进制流
 		InputStream is = responseBody.byteStream();
@@ -29,9 +39,11 @@ class DownloadRetrofitClientTest {
 		if (!tempDirectory.exists()) {
 			tempDirectory.mkdir();
 		}
-		File file = new File(tempDirectory, UUID.randomUUID().toString());
+		File file = new File(tempDirectory, "UUID.randomUUID()".toString()+".jpg");
 		if (!file.exists()) {
 			file.createNewFile();
+		} else {
+			file.delete();
 		}
 		FileOutputStream fos = new FileOutputStream(file);
 		byte[] b = new byte[1024];
