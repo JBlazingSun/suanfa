382ac4d2cb0c77b4c7ad046a5ec067034185c7b5
JBlazingSun <blazings@vip.qq.com>
2022-10-09 19:17:45 +0800

    Default Changelist

--- src/main/java/com/blazings/suanfa/SuanfaApplication.java
+++ src/main/java/com/blazings/suanfa/SuanfaApplication.java
@@ -1,9 +1,6 @@
 package com.blazings.suanfa;
 
-import com.blazings.suanfa.component.retrofit.HelloRetrofitClient;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
