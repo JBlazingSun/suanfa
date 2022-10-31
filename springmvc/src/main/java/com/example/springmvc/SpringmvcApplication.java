8c6e17c91bcb2c43a882024c39c064aa8e8364cd
JBlazingSun <blazings@vip.qq.com>
2022-10-31 18:13:36 +0800

    Default Changelist

--- springmvc/src/main/java/com/example/springmvc/SpringmvcApplication.java
+++ springmvc/src/main/java/com/example/springmvc/SpringmvcApplication.java
@@ -2,9 +2,13 @@ package com.example.springmvc;
 
 import cn.dev33.satoken.quick.SaQuickManager;
 import cn.dev33.satoken.quick.config.SaQuickConfig;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Slf4j
@@ -14,6 +18,11 @@ public class SpringmvcApplication {
 		SpringApplication.run(SpringmvcApplication.class, args);
 		SaQuickConfig config = SaQuickManager.getConfig();
 		log.info(String.valueOf(config));
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> getConfig(@Value("${spring.application.name}") String appName) {
+		return (registry -> registry.config().commonTags("application",appName));
 	}
 
 }
