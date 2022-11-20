32ce574a32d07ce7f1b720335995c1dac5a4f3ae
JBlazingSun <blazings@vip.qq.com>
2022-11-21 00:37:24 +0800

    add

--- springmvc/src/main/java/com/example/springmvc/entity/User.java
+++ springmvc/src/main/java/com/example/springmvc/entity/User.java
@@ -1,12 +1,49 @@
 package com.example.springmvc.entity;
 
 import com.ejlchina.searcher.bean.SearchBean;
-import lombok.Data;
 
-@Data
+import java.io.Serializable;
+
+/**
+ * (User)ʵ����
+ *
+ * @author makejava
+ * @since 2022-11-21 00:29:09
+ */
 @SearchBean
-public class User {
-	private Long id;            // 默认映射到 id 字段
-	private String name;        // 默认映射到 name 字段
-	private int age;            // 默认映射到 age 字段
+public class User implements Serializable {
+    private static final long serialVersionUID = -91785002130377908L;
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
+
