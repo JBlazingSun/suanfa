94157d9e5c8c73a93302df9e53b1c0762d887a38
JBlazingSun <blazings@vip.qq.com>
2022-11-26 02:21:29 +0800

    add

---
+++ springmvc/src/main/java/com/example/springmvc/entity/beansearcher/Q3.java
@@ -0,0 +1,26 @@
+package com.example.springmvc.entity.beansearcher;
+
+import com.ejlchina.searcher.bean.DbField;
+import com.ejlchina.searcher.bean.SearchBean;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@SearchBean(tables = "student s LEFT JOIN score ON score.s_id=s.s_id",
+	groupBy = "s.s_id",
+	having = "AVG(score.s_score)>=60")
+public class Q3 {
+	@DbField(value = "s.s_id")
+	String stuId;
+	@DbField(value = "s.s_name")
+	String stuName;
+	@DbField(value = "AVG(score.s_score)", alias = "avgScore")
+	Double avgScore;
+
+
+	public Double getAvgScore() {
+		BigDecimal decimal = new BigDecimal(avgScore);
+		return decimal.doubleValue();
+	}
+}
--- springmvc/src/test/java/com/example/springmvc/controller/BeanSearcherControllerTest.java
+++ springmvc/src/test/java/com/example/springmvc/controller/BeanSearcherControllerTest.java
@@ -3,11 +3,15 @@ package com.example.springmvc.controller;
 import com.ejlchina.searcher.BeanSearcher;
 import com.example.springmvc.entity.User;
 import com.example.springmvc.entity.beansearcher.Q1;
+import com.example.springmvc.entity.beansearcher.Q3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //https://www.cnblogs.com/Diyo/p/11424844.html
 @SpringBootTest
@@ -16,8 +20,27 @@ class BeanSearcherControllerTest {
 	@Resource
 	BeanSearcher searcher;
 
+	//Q3
+	//3、查询平均成绩大于等于60分的同学的学生编号和学生姓名和平均成绩
+	@Test
+	void name3() {
+//		Map<String, Object> build = MapUtils.builder()
+//			.field(Q3::getStuName, "钱电")
+//			.op(Operator.Equal)
+//			.build();
+		List<Q3> q3s = searcher.searchList(Q3.class, null);
+		List<Q3> q3List = q3s.stream()
+			.map(q3 -> {
+				BigDecimal decimal = new BigDecimal(q3.getAvgScore());
+				q3.setAvgScore(decimal.setScale(0, RoundingMode.HALF_UP).doubleValue());
+				return q3;
+			})
+			.collect(Collectors.toList());
+	}
+
 	@Test
 		//Q1
+		//查询"01"课程比"02"课程成绩高的学生的信息及课程分数
 	void name() {
 		List<Q1> q1s = searcher.searchList(Q1.class, null);
 	}
